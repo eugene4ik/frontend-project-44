e8dba945a4b5dc533660f82e07653cf00f699214
Evgeny Sokol <eugene4ik@mail.com>
2023-06-01 22:39:31 -0600

    fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
 import { userNameFunc } from "../src/cli.mjs";
-import { firstQuestFunc } from "./brain-even.js";
-import { calculator } from "./brain-calc.js";
 
-console.log("hhhkjk");
-//userNameFunc();
-//firstQuestFunc();
-calculator();
+userNameFunc();
